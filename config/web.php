2099b5ad214da7f0079e08207c186b2c9cd7f6fb
root <root@ecs-6017.novalocal>
2018-06-27 10:07:39 +0800

    add vendor

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'zj20180627wxhn',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
