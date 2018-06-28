005f76028e0a6af66a40060029a35ec1b356ae26
William <william_wqq@163.com>
2018-06-28 22:55:44 +0800

    修改

--- helper/SmallWorld.php
+++ helper/SmallWorld.php
@@ -13,4 +13,36 @@ class SmallWorld
 {
     const SUCCESS = 1;
     const ERROR = 0;
+
+    /**
+     * 返回错误
+     * @param string $message
+     * @param array $data
+     * @param int $code
+     * @return string
+     */
+    static public function sendError($message = '', $data = [], $code = self::ERROR) {
+        $data = [
+            'code' => $code,
+            'msg' => $message,
+            'data' => $data
+        ];
+        return json_encode($data);
+    }
+
+    /**
+     * 返回正确
+     * @param string $message
+     * @param array $data
+     * @param int $code
+     * @return string
+     */
+    static public function sendSuccess($message = '', $data = [], $code = self::SUCCESS) {
+        $data = [
+            'code' => $code,
+            'msg' => $message,
+            'data' => $data
+        ];
+        return json_encode($data);
+    }
 }
